f9382f0191ec2a86bf122a5cf5263f0ad3fc1196
JS Deck <jsdeckerido@gmail.com>
2024-07-13 13:10:26 -0300

    poll2: poll without malloc; may change fds even with failure

--- libctru/source/services/soc/soc_poll.c
+++ libctru/source/services/soc/soc_poll.c
@@ -78,3 +78,61 @@ int poll(struct pollfd *fds, nfds_t nfds, int timeout)
 
 	return ret;
 }
+
+int	poll2(struct pollfd *fds, nfds_t nfds, int timeout)
+{
+	int ret = 0;
+	nfds_t i;
+	u32 size = sizeof(struct pollfd)*nfds;
+	u32 *cmdbuf = getThreadCommandBuffer();
+	u32 saved_threadstorage[2];
+
+	if(nfds == 0) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	for(i = 0; i < nfds; ++i) {
+		fds[i].fd = soc_get_fd(fds[i].fd);
+		if(fds[i].fd < 0) {
+			errno = -fds[i].fd;
+			return -1;
+		}
+		fds[i].revents = 0;
+	}
+
+	cmdbuf[0] = IPC_MakeHeader(0x14,2,4); // 0x140084
+	cmdbuf[1] = (u32)nfds;
+	cmdbuf[2] = (u32)timeout;
+	cmdbuf[3] = IPC_Desc_CurProcessId();
+	cmdbuf[5] = IPC_Desc_StaticBuffer(size,10);
+	cmdbuf[6] = (u32)fds;
+
+	u32 * staticbufs = getThreadStaticBuffers();
+	saved_threadstorage[0] = staticbufs[0];
+	saved_threadstorage[1] = staticbufs[1];
+
+	staticbufs[0] = IPC_Desc_StaticBuffer(size,0);
+	staticbufs[1] = (u32)fds;
+
+	ret = svcSendSyncRequest(SOCU_handle);
+
+	staticbufs[0] = saved_threadstorage[0];
+	staticbufs[1] = saved_threadstorage[1];
+
+	if(ret != 0) {
+		errno = SYNC_ERROR;
+		return ret;
+	}
+
+	ret = (int)cmdbuf[1];
+	if(ret == 0)
+		ret = _net_convert_error(cmdbuf[2]);
+
+	if(ret < 0) {
+		errno = -ret;
+		return -1;
+	}
+
+	return ret;
+}
